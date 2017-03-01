d3d0d24624ef29b759813161329bf53cff727f2a
Alex Nelson <caminelson.cn@gmail.com>
2017-02-28 21:09:39 -0800

    controllor reviews

--- baystuff/app/controllers/homepage_controller.rb
+++ baystuff/app/controllers/homepage_controller.rb
@@ -1,7 +1,11 @@
 class HomepageController < ApplicationController
   def index
-    page = HTTParty.get("http://www.nbcbayarea.com/news/local/")
-    parsed_page = Nokogiri::HTML(page)
-    @top_stories = parsed_page.css('.carousel-stories>li')
+    newspage = HTTParty.get("http://www.nbcbayarea.com/news/local/")
+    parsed_newspage = Nokogiri::HTML(newspage)
+    @top_stories = parsed_newspage.css('.carousel-stories>li')
+    
+    eventpage = HTTParty.get("https://www.eventbrite.com/d/ca--san-francisco/events/?mode=search")
+    parsed_eventpage = Nokogiri::HTML(eventpage)
+    # @events = parsed_page.css('')
   end
 end
